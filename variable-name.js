03e20b8ed0e768f201935875c55ebadf2857526c
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-28 12:21:35 -0700

    variable name

---
+++ variable-name.js
@@ -0,0 +1,15 @@
+// 1. variable name cannot be any keyword
+
+// 2. variable name has to be in one work . No space
+
+// 3. variable name can not have quotation
+
+// 4. variable name can not be start With a number but can ends with a number
+
+// 5. how to use long names 
+// can not use dash
+var user_name = "Zufer Khan"; //snake case
+var userHomeAddress = "Bangladesh"; //camel case
+var UserHomeAddress = "Dhaka"; //pascal case
+
+// 6. variable name is case sensitive
