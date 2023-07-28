ab30d63aa7177fef27115aa983d977c3db8f5184
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-28 11:53:19 -0700

    numeric string and boolean variable

--- variable.js
+++ variable.js
@@ -1,7 +1,18 @@
 // vary + able = variable
 var age = 20;
+// numeric
 var price = 520;
 var temperature = 38;
-var name = "Mazharul"
 var wight = 60;
-var mony = 1050;
+var money = 1050;
+
+// string
+var person = "Mazharul Islam";
+var location = "Mymenshingh";
+var special = "Ali bhatt"
+
+// Boolean
+var series = true;
+var isFullMarks = true;
+var isSingle = false;
+
