889f522e1203db5411735836a3c32f644b1ad15d
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-28 13:00:49 -0700

    opeerator in js

---
+++ operation.js
@@ -0,0 +1,30 @@
+
+var onionPrice = 35;
+var eegPrice = 45;
+
+// addition 
+// var totalPrice = onionPrice+eegPrice;
+// console.log(totalPrice);
+
+// substraction
+var priceDifference = onionPrice- eegPrice;
+console.log(priceDifference);
+
+
+// console.log(onionPrice);
+// console.log(eegPrice);
+// console.log(onionPrice+eegPrice);
+
+
+// multplication
+var orangeprice = 20;
+var quantiy = 7;
+var totalCost = orangeprice * quantiy;
+console.log(totalCost);
+
+
+// devision
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
