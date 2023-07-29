328bd16e4d8d69c49c64617403c2bfc80c8d0f7e
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-28 22:34:27 -0700

    conversion and concanaction in js

---
+++ conversion1.js
@@ -0,0 +1,18 @@
+var price1 = '31';
+var price2 = '10';
+var totalPrice = price1+price2;
+console.log(totalPrice);
+console.log(price1);
+
+
+
+// convert string to number 
+var price1Number = parseInt(price1);
+console.log(price1Number);
+
+
+// float 
+var gpa = '3.54';
+var gpaNumber = parseFloat(gpa);
+console.log(gpaNumber);
+
