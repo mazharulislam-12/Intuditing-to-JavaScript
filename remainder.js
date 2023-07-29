c8a58381e839affb279bf77dfb5cbf42af00c7f6
mazharulislam-12 <505mazharulislam@gmail.com>
2023-07-28 23:02:55 -0700

    types of and reminder

---
+++ remainder.js
@@ -0,0 +1,6 @@
+var mangos = 11;
+var person = 3;
+var remainder = mangos % person;
+//  / this means vagfol
+// % this means vagses
+console.log(remainder);
---
+++ types.js
@@ -0,0 +1,19 @@
+// number
+var price = 100;
+console.log(typeof price);
+// string
+var price1 = "102";
+console.log(typeof price1);
+
+// boolean
+var isHappy = true;
+console.log(typeof isHappy);
+
+var num1 = 0.1;
+var num2 = 0.2;
+sum = num1 + num2;
+sum = sum.toFixed(2)
+sum = parseFloat(sum)
+console.log(sum);
+
+
